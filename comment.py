c2db68b3a3a7352bcc9eedbf41521945cbb62fc0
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2022-11-26 13:03:53 +0530

    Add files via upload

---
+++ comment.py
@@ -0,0 +1,29 @@
+#this is first coding in coding
+# Comment: this is line that is not read by python
+print("hello world.")
+"""
+this is Nilesh kumar
+from lovely professinal university
+B.tecch computer science and engineering.
+"""
+print("this is nilesh","from lovely professional university")
+
+#print("this is nilesh")
+#print("from lovely professional university")
+
+#print("C:\"NILESH")
+
+print("I am doing B.tech cse \n from lpu\t")
+
+
+print("I am learning coding \r in block 38 206!")
+
+
+
+
+#ESCAPE CHARACTERS!!!!
+
+# \\ : backlash
+# \n: New line
+# \t : New tab
+# \b : Backspace
---
+++ varibles.py
@@ -0,0 +1,44 @@
+#a = "hello world"
+#b = 24
+#c =45.1
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+
+#var1 = "34"
+#var2 = 34
+#print(int(var1)+ var2)
+#print(10  * "Hello world \n")
+
+
+#print("Enter your name")
+
+#num = input()
+#print("You entered", num)
+
+
+# PROJECT : CALULATOR USED FOR ADDITION TWO NUMBER.
+
+
+#print("Enter the first number")
+n1 = int(input())
+#print("Enter the second number")
+n2 = int(input())
+s = n1 + n2
+#print("Sum of two number:", s )
+
+
+
+print("Enter the  first number")
+num1= int(input())
+print("Enter the second number")
+num2= int(input())
+r = num1 + num2
+q = num1 - num2
+x = num1 * num2
+f = num1 / num2
+print("sum of the number:", r)
+print("subtract of the number:", q)
+print("multiply of the number:", x)
+print("division of the number:", f)
