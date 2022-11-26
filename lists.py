6a4e42de122f38bc56a627d2d278c4e7413d9fd6
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2022-11-26 23:00:03 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,46 @@
+var1 = ["Nilesh","himanshu","kumar", "Cipherschools",43]
+#print(var1)
+#print(var1[3])
+
+numbers = [31,45,76,34,8,]
+#numbers.sort()
+#numbers.reverse()
+#print(numbers[1:4])
+#numbers.append(43454)
+#numbers.insert(2,68769)
+#numbers.remove(76)
+#print(numbers)
+#numbers[3] = 666
+#print(numbers)
+
+
+
+# ***** tuples are immutable while  lists are mutable!
+#tp = (1,)
+##print(tp)
+
+
+a = 3
+b = 676
+a,b = b,a
+print(a,b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
